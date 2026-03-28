b804af0479153da2aab4ac6c3458ac39860cbf59
Efereank <franciscojavier20006@gmail.com>
2026-03-27 23:24:00 -0400

    change

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import imagemin from 'vite-plugin-imagemin';
 
 export default defineConfig({
     plugins: [
@@ -8,41 +7,5 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        imagemin({
-            // Compresión de imágenes
-            gifsicle: {
-                optimizationLevel: 3,
-            },
-            mozjpeg: {
-                quality: 80,
-            },
-            pngquant: {
-                quality: [0.7, 0.8],
-            },
-            webp: {
-                quality: 80,
-            },
-        }),
     ],
-    build: {
-        minify: 'terser',
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    vendor: ['alpinejs'],
-                },
-            },
-        },
-        assetsInlineLimit: 4096,
-        cssCodeSplit: true,
-        sourcemap: false,
-    },
-    server: {
-        hmr: {
-            overlay: true,
-        },
-    },
-    optimizeDeps: {
-        include: ['alpinejs'],
-    },
 });
