30adc7b810ba8d6966397821ee9b8f5699ae66ee
Efereank <franciscojavier20006@gmail.com>
2026-03-28 23:15:24 -0400

    commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Noticaimán') }}</title>
 
-    <!-- Preconnect para fuentes (mejora velocidad) -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Bangers&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
@@ -14,8 +13,6 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    <!-- Estilos adicionales -->
     @stack('styles')
 </head>
 <body class="site-body">
